407dbe21729431617be1624a8dfd311b4fb5bf83
Jerich <jerichmalcolm@gmail.com>
2016-11-03 22:13:06 +0800

    Refactor countdown method

--- src/main/java/seedu/Tdoo/ui/DeadlineCard.java
+++ src/main/java/seedu/Tdoo/ui/DeadlineCard.java
@@ -168,36 +168,49 @@ public class DeadlineCard extends UiPart{
             date.setText("Date: " + task.getStartDate().date);
             endTime.setText("End Time: " + task.getEndTime().endTime);
             done.setText("Completed");
-            countdown.setText(p.format(new Date(System.currentTimeMillis() + dateToMilli(task.getStartDate().date, task.getEndTime().endTime))));
+            countdown.setText(convertDateToMilli(task.getStartDate().date, task.getEndTime().endTime));
             cardPane.setStyle("-fx-background-color: #01DF01");
         }else if(!checkEndDateTime() && this.task.getDone().equals("false")) {
             date.setText("Date: " + task.getStartDate().date);
             endTime.setText("End Time: " + task.getEndTime().endTime);
             done.setText("Overdue");
-            countdown.setText(p.format(new Date(System.currentTimeMillis() + dateToMilli(task.getStartDate().date, task.getEndTime().endTime))));
+            countdown.setText(convertDateToMilli(task.getStartDate().date, task.getEndTime().endTime));
             cardPane.setStyle("-fx-background-color: #ff2002");
         }else {
             date.setText("Date: " + task.getStartDate().date);
             endTime.setText("End Time: " + task.getEndTime().endTime);
             done.setText("Not Completed");
-            countdown.setText(p.format(new Date(System.currentTimeMillis() + dateToMilli(task.getStartDate().date, task.getEndTime().endTime))));
+            countdown.setText(convertDateToMilli(task.getStartDate().date, task.getEndTime().endTime));
             cardPane.setStyle("-fx-background-color: #FFFFFF");
         }
     }     
-    //@@author A0132157M
-    public long dateToMilli(String s, String q) throws ParseException {
+  //@@author A0132157M
+    public String convertDateToMilli(String s, String q) throws ParseException {
         String string = s;
         String[] parts = string.split(" ");
         String part1 = parts[0]; 
         String part2 = parts[1]; 
         String part3 = parts[2];
-        String parta = part1.substring(0, 2);
+        String part4 = part1.substring(0, 2);
         String bstring = q;
-        String input = part3 + " " + part2 + " " + parta + " " + bstring;
+        String input = part3 + " " + part2 + " " + part4 + " " + bstring;
+        String result = convertToSDF(input);
+        return result;
+    }
+    
+    //@@author A0132157M
+    public String convertToSDF(String input) throws ParseException {
         Date date = new SimpleDateFormat("yyyy MMMM dd HH:mmaaa", Locale.ENGLISH).parse(input);
         long milliseconds = date.getTime();
-        long ss = milliseconds - (new Date()).getTime();
-        return ss;
+        long dateToMilli = milliseconds - (new Date()).getTime();
+        String result = setPrettyTime(dateToMilli);
+        return result;
+    }
+    
+    //@@author A0132157M
+    public String setPrettyTime(long input) {
+        String result = p.format(new Date(System.currentTimeMillis() + input));
+        return result; 
     }
 
     public HBox getLayout() {
